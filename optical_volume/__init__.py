ecfa202d16a9ea7d94e0d0b1b5d71ecda147d033
Syed Kazim <syed.kazim@uni-siegen.de>
2026-04-20 15:49:37 +0200

    re/moved in refactoring

--- optical_volume/__init__.py
+++
@@ -1,9 +0,0 @@
-# my_package/__init__.py
-from . import geometry
-from . import torch_geometry
-from . import torch_cwfs
-from . import propagator
-from . import visualization
-from . import utils
-
-__all__ = ["geometry", "torch_geometry", "torch_cwfs", "propagator", "visualization", "utils"]
