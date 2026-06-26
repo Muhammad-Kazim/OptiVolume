8d13550113abeb4f7f1d7d49df1b61d3c105aef6
Syed Kazim <syed.kazim@uni-siegen.de>
2026-06-26 17:33:01 +0200

    magnification operator added

--- src/optical_volume/optics/utils.py
+++ src/optical_volume/optics/utils.py
@@ -1,4 +1,6 @@
 from torch import nn
+import torchvision
+from torchvision.transforms.functional import resize
 
 
 class Modulator(nn.Module):
@@ -9,3 +11,19 @@ class Modulator(nn.Module):
         
     def forward(self, field):
         return self.operator*field
+    
+    
+class Magnification(nn.Module):
+    def __init__(self, mag_factor):
+        super().__init__()
+        
+        self.mag_factor = mag_factor
+        
+    def forward(self, field):
+        field_real = resize(field.unsqueeze(0).real, field.shape[0]*self.mag_factor, torchvision.transforms.InterpolationMode.BILINEAR, antialias=True).squeeze()
+        field_imag = resize(field.unsqueeze(0).imag, field.shape[0]*self.mag_factor, torchvision.transforms.InterpolationMode.BILINEAR, antialias=True).squeeze()
+        
+        return field_real + 1j*field_imag
+    
+    def set_mag(self, mag_factor):
+        self.mag_factor = mag_factor
