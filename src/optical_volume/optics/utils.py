b93b9ebe1fbface6dbaab96eeea833c2b4712535
Syed Kazim <syed.kazim@uni-siegen.de>
2026-07-28 12:36:59 +0200

    added pixel integration to convert aerial to difital pixels

--- src/optical_volume/optics/utils.py
+++ src/optical_volume/optics/utils.py
@@ -1,9 +1,13 @@
-from torch import nn
+import torch
+from torch import nn, Tensor
 import torchvision
 from torchvision.transforms.functional import resize
 
 
 class Modulator(nn.Module):
+    """
+    Makes transmission functions compatible with system.OpticalSystem module.
+    """
     def __init__(self, operator):
         super().__init__()
         
@@ -17,7 +21,7 @@ class Magnification(nn.Module):
     def __init__(self, mag_factor):
         super().__init__()
         
-        self.mag_factor = mag_factor
+        self.mag_factor = self.register_buffer('mag_factor', mag_factor)
         
     def forward(self, field):
         field_real = resize(field.unsqueeze(0).real, field.shape[0]*self.mag_factor, torchvision.transforms.InterpolationMode.BILINEAR, antialias=True).squeeze()
@@ -27,3 +31,16 @@ class Magnification(nn.Module):
     
     def set_mag(self, mag_factor):
         self.mag_factor = mag_factor
+        return self
+
+class PixelIntegrator(nn.Module):
+    def __init__(self, stride: int):
+        super().__init__()
+        
+        self.conv2d = nn.Conv2d(1, 1, stride, self.sum_size.int().item(), bias=False)
+        
+        self.conv2d.weight = nn.Parameter(torch.ones([stride, stride]).unsqueeze(0).unsqueeze(0)/stride**2)
+        self.conv2d.weight.requires_grad = False
+        
+    def forward(self, field: Tensor):
+        return self.conv2d(field.unsqueeze(0)).squeeze()
